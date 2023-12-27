91bc1d5aac01dae0bed4e9de5739769adbadccd3
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2023-12-26 19:19:00 -0800

    dynamic answer calling

--- lib/aoc23.rb
+++ lib/aoc23.rb
@@ -5,8 +5,18 @@ Dir[File.expand_path("lib/**/day/**/*.rb")].each { |file| require file }
 class Aoc23
   class << self
     def print_answers
-      puts("Day 1: ")
-      puts(Day01.call)
+      (1..last_completed_day).map { |i| format("%02d", i) }.each do |day|
+        puts("Day " + day.to_i.to_s)
+        puts(Module.const_get("Day#{day}").call)
+        puts("=" * 10)
+      end
+    end
+
+    private
+
+    def last_completed_day
+      # lol this is inefficient
+      Dir[File.expand_path("lib/**/day/**/")].map { |path| path[-2, 2] }.last.to_i
     end
   end
 end
